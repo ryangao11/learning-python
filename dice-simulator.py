680b138d40cd17a072e88c9f0fd55fa3612759dd
Ryan Gao <rmgao@princeton.edu>
2017-12-18 20:54:20 -0500

    built out dice-simulator

--- dice-simulator.py
+++ dice-simulator.py
@@ -1,7 +1,17 @@
 import sys
 import random
 def main():
-	print random.randint(1,7)
-
+	if len(sys.argv) >= 2:
+		number = int(sys.argv[1])
+	else:
+		number = 1
+	i = 1
+	sum = 0
+	while i <= number: 
+		roll = random.randint(1,6)
+		print 'Roll', i, 'is:', roll
+		sum = sum + roll
+		i = i + 1
+	print 'Average of the rolls is: ', float(sum)/number
 if __name__ == '__main__':
   main()
