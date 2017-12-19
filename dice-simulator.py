72ad2274bcbbc65ed23155381ef27c4897fbe7ec
Ryan Gao <rmgao@princeton.edu>
2017-12-18 20:09:12 -0500

    first programs

---
+++ dice-simulator.py
@@ -0,0 +1,7 @@
+import sys
+import random
+def main():
+	print random.randint(1,7)
+
+if __name__ == '__main__':
+  main()
---
+++ hello.py
@@ -0,0 +1,8 @@
+import sys
+# Define a main() function
+def main():
+  print 'Hello World'
+
+# Call the main() function.
+if __name__ == '__main__':
+  main()
